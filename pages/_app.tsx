03666e2ac919ebead003ed41cefc7eea6eaa3047
Resi Respati <resir014@gmail.com>
2019-02-12 01:36:33 +0700

    fixed styles not hydrating in document component

---
+++ pages/_app.tsx
@@ -0,0 +1,28 @@
+import App, { Container, NextAppContext } from 'next/app'
+import Head from 'next/head'
+
+export default class MyApp extends App {
+  static async getInitialProps({ Component, ctx }: NextAppContext) {
+    let pageProps = {}
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx)
+    }
+
+    return { pageProps }
+  }
+
+  render() {
+    const { Component, pageProps } = this.props
+
+    return (
+      <Container>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <title>Next.js TypeScript Quickstart</title>
+        </Head>
+        <Component {...pageProps} />
+      </Container>
+    )
+  }
+}
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,18 @@
-import Document, { Head, Main, NextScript } from 'next/document'
+import Document, { Head, Main, NextScript, NextDocumentContext } from 'next/document'
+import flush from 'styled-jsx/server'
 
 export default class MyDocument extends Document {
+  public static async getInitialProps(ctx: NextDocumentContext) {
+    const { html, head, buildManifest } = ctx.renderPage()
+    const styles = flush()
+    return { html, head, styles, buildManifest }
+  }
+
   public render() {
     return (
       <html lang="en">
         <Head>
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <title>Next.js TypeScript Quickstart</title>
+          <style>{this.props.styles}</style>
         </Head>
         <body>
           <Main />
