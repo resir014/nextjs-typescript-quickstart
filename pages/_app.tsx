5932f7d1cbc22fac26250ca68fd8ee1eb0cae5c0
Resi Respati <resir014@gmail.com>
2019-07-18 14:24:19 +0700

    remove getInitialProps in App to enable static export detection

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,16 +3,6 @@ import App, { Container, AppContext } from 'next/app'
 import Head from 'next/head'
 
 export default class MyApp extends App {
-  public static async getInitialProps({ Component, ctx }: AppContext) {
-    let pageProps = {}
-
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx)
-    }
-
-    return { pageProps }
-  }
-
   public render() {
     const { Component, pageProps } = this.props
 
