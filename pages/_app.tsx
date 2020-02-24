36bf10eff683b5b0b2b8ef1ad47a9fbdcca8f789
Resi Respati <resir014@gmail.com>
2020-02-24 20:43:00 +0700

    remove deprecated Container component from app page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import App, { Container } from 'next/app'
+import App from 'next/app'
 import Head from 'next/head'
 
 import 'modern-normalize'
@@ -9,13 +9,13 @@ export default class MyApp extends App {
     const { Component, pageProps } = this.props
 
     return (
-      <Container>
+      <>
         <Head>
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <title>Next.js TypeScript Quickstart</title>
         </Head>
         <Component {...pageProps} />
-      </Container>
+      </>
     )
   }
 }
