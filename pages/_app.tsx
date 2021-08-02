b85a973b50de460999d618d609ae438aa0c6f9cd
Resi Respati <resir014@gmail.com>
2021-08-02 23:39:32 +0700

    simplify example page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,13 @@ import { AppProps } from 'next/app'
 import Head from 'next/head'
 
 import 'modern-normalize'
+import '../styles/globals.css'
 
 export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Next.js TypeScript Quickstart</title>
       </Head>
       <Component {...pageProps} />
     </>
