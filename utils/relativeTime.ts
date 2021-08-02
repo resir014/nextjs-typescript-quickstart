71d28dab1932a96c9b72f84fe864324a71234438
Resi Respati <resir014@gmail.com>
2021-08-02 23:46:43 +0700

    simplify example page (#9)

--- utils/relativeTime.ts
+++
@@ -1,7 +0,0 @@
-import formatDistance from 'date-fns/formatDistance'
-
-function relativeTime(date: Date): string {
-  return `${formatDistance(Date.now(), date)} ago`
-}
-
-export default relativeTime
